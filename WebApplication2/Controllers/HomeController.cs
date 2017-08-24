327283c54d3ee4299fad3697e1f40f72ec4f76a6
dilara.kaymak <dilara.kaymak@morhipo.com>
2017-08-24 10:10:31 +0300

    Deneme commit

--- WebApplication2/Controllers/HomeController.cs
+++ WebApplication2/Controllers/HomeController.cs
@@ -14,6 +14,7 @@ namespace WebApplication2.Controllers
         B301_BlogEntities context = new B301_BlogEntities();
         public ActionResult Index()
         {
+
             return View();
         }
 
