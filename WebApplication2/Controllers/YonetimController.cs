bb266d5aec12c1c16fb960070c825608ad3048ab
dilara.kaymak <dilara.kaymak@morhipo.com>
2017-08-24 18:21:27 +0300

    24.08.2017

--- WebApplication2/Controllers/YonetimController.cs
+++ WebApplication2/Controllers/YonetimController.cs
@@ -9,9 +9,23 @@ namespace WebApplication2.Controllers
     public class YonetimController : Controller
     {
         // GET: Yonetim
+        B301_BlogEntities context = new B301_BlogEntities();
         public ActionResult Index()
+        {
+            ViewBag.Tip = 1;
+            return View();
+        }
+
+        public ActionResult MakaleYaz()
+        {
+            ViewBag.Tip = 1;
+            ViewBag.Kategoriler = context.Kategori.ToList();
+            return View();
+        }
+        [HttpPost]
+        public ActionResult MakaleYaz(Makale makale,HttpPostedFileBase Resim, string etiketler)
         {
             return View();
         }
-    }
+    }   
 }
