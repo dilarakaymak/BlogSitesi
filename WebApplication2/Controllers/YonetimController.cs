182d569508d57d9892bfad2147cb006c1342c7ee
dilara.kaymak <dilara.kaymak@morhipo.com>
2017-08-25 17:24:32 +0300

    25.08.2017

--- WebApplication2/Controllers/YonetimController.cs
+++ WebApplication2/Controllers/YonetimController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -25,7 +26,26 @@ namespace WebApplication2.Controllers
         [HttpPost]
         public ActionResult MakaleYaz(Makale makale,HttpPostedFileBase Resim, string etiketler)
         {
+            if (makale != null)
+            {
+                Kulllanici aktif = Session["Kullanici"] as Kulllanici; 
+                makale.YayimTarihi = DateTime.Now;
+                makale.MakaleTypeID = 1;
+                makale.YazarID = aktif.id;
+                makale.KapakResimID = ResimKaydet(Resim);
+            }
             return View();
+        }
+
+        private int ResimKaydet(HttpPostedFileBase resim)
+        {
+            int kucukWidght = Convert.ToInt32(ConfigurationManager.AppSettings["kw"]);
+            int kucukHeight = Convert.ToInt32(ConfigurationManager.AppSettings["kh"]);
+            int ortaWidght = Convert.ToInt32(ConfigurationManager.AppSettings["ow"]);
+            int ortaHeight = Convert.ToInt32(ConfigurationManager.AppSettings["oh"]);
+            int buyukWidght = Convert.ToInt32(ConfigurationManager.AppSettings["bw"]);
+            int buyukHeight = Convert.ToInt32(ConfigurationManager.AppSettings["bh"]);
+
         }
     }   
 }
